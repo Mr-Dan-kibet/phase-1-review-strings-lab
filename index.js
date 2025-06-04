2ac1b0cbf97064cbbb862b15085a8aa3ec420534
Mr-Dan-kibet <dan.rotich@student.moringaschool.com>
2025-06-04 15:49:25 +0300

    applying concatenation and interpolation

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Dan rotich";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "DAN ROTICH" + `!`;
+let shortGreeting = "Welcome, D!";
